7af91096cf6d7c08a38f57ad62680e3d7865f136
unknown <>
2017-03-06 17:23:10 +0800

    使用thinkphp框架 Signed-off-by: unknown <陈天豪>

---
+++ Application/Runtime/Cache/Home/4913b3ab5f520f6dd350e21137b73fb2.php
@@ -0,0 +1,5 @@
+<?php if (!defined('THINK_PATH')) exit();?><form method="post" action="Excel/add" enctype="multipart/form-data">
+    <h3>导入Excel表：</h3><input  type="file" name="file_stu" />
+
+    <input type="submit"  value="导入" />
+</form>
